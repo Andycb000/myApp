94664173a6c60e7fd01534c3a40a989a0a63dcd5
Andycb000 <19030@portalcedi.edu.mx>
2025-01-13 11:37:45 -0600

    upcoming events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,40 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MembersComponent } from './members/members.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { UpcomingComponent } from './upcoming/upcoming.component';
+import { SongsComponent } from './songs/songs.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'members',
+    component: MembersComponent,
+  },
+  {
+    path: 'albums',
+    component: AlbumsComponent,
+  },
+  {
+    path: 'upcoming',
+    component: UpcomingComponent,
+  },
+  {
+    path: 'songs',
+    component: SongsComponent,
   },
 ];
 
